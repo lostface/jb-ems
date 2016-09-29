ab65c5ca5229ec29bd77165be8f59c5bea06c050
Zoltan Feher <lostface@fastmail.fm>
2016-09-29 14:18:07 +0200

    Add failing unit tests for calculateDueDate when submit date is invalid

---
+++ index.test.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const R = require('ramda');
+const index = require('./index');
+const calculateDueDate = index.calculateDueDate;
+const test = require('tape');
+
+// const WEEK_DAY_STRS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
+// const WEEKEND_DAY_STRS = ['Sat', 'Sun'];
+const DUMMY_TURNAROUND_TIME = 8;
+
+test('calculateDueDate call with invalid submit dates', function(t) {
+  const paramsArr = getParams();
+  R.forEach(runTests, paramsArr);
+  t.end();
+
+  function runTests(params) {
+    const submitDate = params.submitDate;
+    const submitDateStr = new Date(submitDate);
+    const turnaroundTime = DUMMY_TURNAROUND_TIME;
+
+    t.test(`calculateDueDate call with invalid submit date (${submitDateStr})`, function(pt) {
+      pt.throws(
+        () => calculateDueDate(submitDate, turnaroundTime),
+        /Error: Invalid submitDate parameter\. Submit date should be a working day \(Mon to Fri, 9:00 to 17:00\)/,
+        'calculateDueDate() should throw exception when called with invalid submit date'
+      );
+
+      pt.end();
+    });
+  }
+
+  function getParams() {
+    return [
+      // week days
+      {
+        submitDate: getTime('Mon, 26 Sep 2016 8:59'),
+      },
+      {
+        submitDate: getTime('Mon, 26 Sep 2016 17:01'),
+      },
+      {
+        submitDate: getTime('Tue, 27 Sep 2016 8:59'),
+      },
+      {
+        submitDate: getTime('Tue, 27 Sep 2016 17:01'),
+      },
+      {
+        submitDate: getTime('Wed, 28 Sep 2016 8:59'),
+      },
+      {
+        submitDate: getTime('Wed, 28 Sep 2016 17:01'),
+      },
+      {
+        submitDate: getTime('Thu, 29 Sep 2016 8:59'),
+      },
+      {
+        submitDate: getTime('Thu, 29 Sep 2016 17:01'),
+      },
+      {
+        submitDate: getTime('Fri, 30 Sep 2016 8:59'),
+      },
+      {
+        submitDate: getTime('Fri, 30 Sep 2016 17:01'),
+      },
+
+      // weeken days
+      {
+        submitDate: getTime('Sat, 1 Oct 2016 8:59'),
+      },
+      {
+        submitDate: getTime('Sat, 1 Oct 2016 12:00'),
+      },
+      {
+        submitDate: getTime('Sat, 1 Oct 2016 17:01'),
+      },
+      {
+        submitDate: getTime('Sun, 2 Oct 2016 8:59'),
+      },
+      {
+        submitDate: getTime('Sun, 2 Oct 2016 12:00'),
+      },
+      {
+        submitDate: getTime('Sun, 2 Oct 2016 17:01'),
+      },
+    ];
+  }
+});
+
+function getTime(dateStr) {
+  return new Date(dateStr).getTime();
+}
