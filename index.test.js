f65aee87403226519d25ee6abb6c2b4bb48d8923
Zoltan Feher <lostface@fastmail.fm>
2016-09-29 15:31:09 +0200

    Switch to GMT times in tests

--- index.test.js
+++ index.test.js
@@ -17,7 +17,7 @@ test('calculateDueDate called with invalid submit dates', function(t) {
 
   function runTests(params) {
     const submitTimestamp = params.submitTimestamp;
-    const submitDateStr = new Date(submitTimestamp).toString();
+    const submitDateStr = new Date(submitTimestamp).toGMTString();
     const turnaroundTime = DUMMY_TURNAROUND_TIME;
 
     t.test(`calculateDueDate called with invalid submit date (${submitDateStr})`, function(pt) {
@@ -35,54 +35,54 @@ test('calculateDueDate called with invalid submit dates', function(t) {
     return [
       // week days
       {
-        submitTimestamp: getTime('Mon, 26 Sep 2016 8:59'),
+        submitTimestamp: getGmtTime('Mon, 26 Sep 2016 8:59'),
       },
       {
-        submitTimestamp: getTime('Mon, 26 Sep 2016 17:01'),
+        submitTimestamp: getGmtTime('Mon, 26 Sep 2016 17:01'),
       },
       {
-        submitTimestamp: getTime('Tue, 27 Sep 2016 8:59'),
+        submitTimestamp: getGmtTime('Tue, 27 Sep 2016 8:59'),
       },
       {
-        submitTimestamp: getTime('Tue, 27 Sep 2016 17:01'),
+        submitTimestamp: getGmtTime('Tue, 27 Sep 2016 17:01'),
       },
       {
-        submitTimestamp: getTime('Wed, 28 Sep 2016 8:59'),
+        submitTimestamp: getGmtTime('Wed, 28 Sep 2016 8:59'),
       },
       {
-        submitTimestamp: getTime('Wed, 28 Sep 2016 17:01'),
+        submitTimestamp: getGmtTime('Wed, 28 Sep 2016 17:01'),
       },
       {
-        submitTimestamp: getTime('Thu, 29 Sep 2016 8:59'),
+        submitTimestamp: getGmtTime('Thu, 29 Sep 2016 8:59'),
       },
       {
-        submitTimestamp: getTime('Thu, 29 Sep 2016 17:01'),
+        submitTimestamp: getGmtTime('Thu, 29 Sep 2016 17:01'),
       },
       {
-        submitTimestamp: getTime('Fri, 30 Sep 2016 8:59'),
+        submitTimestamp: getGmtTime('Fri, 30 Sep 2016 8:59'),
       },
       {
-        submitTimestamp: getTime('Fri, 30 Sep 2016 17:01'),
+        submitTimestamp: getGmtTime('Fri, 30 Sep 2016 17:01'),
       },
 
       // weeken days
       {
-        submitTimestamp: getTime('Sat, 1 Oct 2016 8:59'),
+        submitTimestamp: getGmtTime('Sat, 1 Oct 2016 8:59'),
       },
       {
-        submitTimestamp: getTime('Sat, 1 Oct 2016 12:00'),
+        submitTimestamp: getGmtTime('Sat, 1 Oct 2016 12:00'),
       },
       {
-        submitTimestamp: getTime('Sat, 1 Oct 2016 17:01'),
+        submitTimestamp: getGmtTime('Sat, 1 Oct 2016 17:01'),
       },
       {
-        submitTimestamp: getTime('Sun, 2 Oct 2016 8:59'),
+        submitTimestamp: getGmtTime('Sun, 2 Oct 2016 8:59'),
       },
       {
-        submitTimestamp: getTime('Sun, 2 Oct 2016 12:00'),
+        submitTimestamp: getGmtTime('Sun, 2 Oct 2016 12:00'),
       },
       {
-        submitTimestamp: getTime('Sun, 2 Oct 2016 17:01'),
+        submitTimestamp: getGmtTime('Sun, 2 Oct 2016 17:01'),
       },
     ];
   }
@@ -95,7 +95,7 @@ test('isWorkingDay output', function(t) {
 
   function runTests(param) {
     const timestamp = param.timestamp;
-    const dateStr = new Date(timestamp).toString();
+    const dateStr = new Date(timestamp).toGMTString();
     const expected = param.expected;
 
     t.test(`isWorkingDay output when called with (${dateStr})`, function(pt) {
@@ -108,37 +108,37 @@ test('isWorkingDay output', function(t) {
   function getParams() {
     return [
       {
-        timestamp: getTime('Mon, 26 Sep 2016'),
+        timestamp: getGmtTime('Mon, 26 Sep 2016'),
         expected: true,
       },
       {
-        timestamp: getTime('Tue, 27 Sep 2016'),
+        timestamp: getGmtTime('Tue, 27 Sep 2016'),
         expected: true,
       },
       {
-        timestamp: getTime('Wed, 28 Sep 2016'),
+        timestamp: getGmtTime('Wed, 28 Sep 2016'),
         expected: true,
       },
       {
-        timestamp: getTime('Thu, 29 Sep 2016'),
+        timestamp: getGmtTime('Thu, 29 Sep 2016'),
         expected: true,
       },
       {
-        timestamp: getTime('Fri, 30 Sep 2016'),
+        timestamp: getGmtTime('Fri, 30 Sep 2016'),
         expected: true,
       },
       {
-        timestamp: getTime('Sat, 1 Oct 2016'),
+        timestamp: getGmtTime('Sat, 1 Oct 2016'),
         expected: false,
       },
       {
-        timestamp: getTime('Sun, 2 Oct 2016'),
+        timestamp: getGmtTime('Sun, 2 Oct 2016'),
         expected: false,
       },
     ];
   }
 });
 
-function getTime(dateStr) {
-  return new Date(dateStr).getTime();
+function getGmtTime(dateStr) {
+  return new Date(`${dateStr} GMT`).getTime();
 }
